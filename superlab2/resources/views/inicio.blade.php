d64a581b69bf417ce8f757062b82c2d2696c0962
Bosco <bosco11jico100@gmail.com>
2021-06-29 10:38:36 -0600

    Update inicio.blade.php

--- superlab2/resources/views/inicio.blade.php
+++ superlab2/resources/views/inicio.blade.php
@@ -77,11 +77,95 @@
                     <div id="myTabContent" class="tab-content">
 
                         <div class="tab-pane container active" id="service-one" style="font-size: 18px;">
-                            <h1>HOLA1</h1>
+                            <div class="jumbotron jumbotron-sm">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-sm-12 col-lg-12">
+                                            <h1 class="h1" id="titulo1">
+                                                Titulo </h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-ms-8">
+                                        <div class="well well-sm">
+                                            <form>
+                                                <div class="row">
+                                                    <div class="d-flex">
+                                                        <div class="form-group">
+                                                            <h5><label class="col-1-left" for="">
+                                                                    Descripción:
+                                                                </label></h5>
+                                                            <label style="text-align:justify;" for="">Al contrario del pensamiento popular,
+                                                                el texto de Lorem Ipsum no es simplemente texto aleatorio.
+                                                                Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,
+                                                                haciendo que este adquiera mas de 2000 años de antiguedad. Richard McClintock,
+                                                                un profesor de Latin de la Universidad de Hampden-Sydney en Virginia,
+                                                                encontró una de las palabras más oscuras de la lengua del latín, "consecteur", en un pasaje de Lorem Ipsum,
+                                                                y al seguir leyendo distintos textos del latín, descubrió la fuente indudable.
+                                                                Lorem Ipsum viene de las secciones 1.10.32 y 1.10.33 de "de Finnibus Bonorum et Malorum"
+                                                                (Los Extremos del Bien y El Mal) por Cicero, escrito en el año 45 antes de Cristo.
+                                                                Este libro es un tratado de teoría de éticas, muy popular durante el Renacimiento.
+                                                                La primera linea del Lorem Ipsum, "Lorem ipsum dolor sit amet..",
+                                                                viene de una linea en la sección 1.10.32 El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados.
+                                                                Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta,
+                                                                acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.</label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="tab-pane container" id="service-two" style="font-size: 18px;">
-                            <h1>HOLA2</h1>
+                            <div class="jumbotron jumbotron-sm">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-sm-12 col-lg-12">
+                                            <h1 class="h1" id="titulo1">
+                                                Titulo </h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-ms-8">
+                                        <div class="well well-sm">
+                                            <form>
+                                                <div class="row">
+                                                    <div class="d-flex">
+                                                        <div class="form-group">
+                                                            <h5><label for="">
+                                                                    Descripción:
+                                                                </label></h5>
+                                                            <label style="text-align:justify;" for="">Al contrario del pensamiento popular,
+                                                                el texto de Lorem Ipsum no es simplemente texto aleatorio.
+                                                                Tiene sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,
+                                                                haciendo que este adquiera mas de 2000 años de antiguedad. Richard McClintock,
+                                                                un profesor de Latin de la Universidad de Hampden-Sydney en Virginia,
+                                                                encontró una de las palabras más oscuras de la lengua del latín, "consecteur", en un pasaje de Lorem Ipsum,
+                                                                y al seguir leyendo distintos textos del latín, descubrió la fuente indudable.
+                                                                Lorem Ipsum viene de las secciones 1.10.32 y 1.10.33 de "de Finnibus Bonorum et Malorum"
+                                                                (Los Extremos del Bien y El Mal) por Cicero, escrito en el año 45 antes de Cristo.
+                                                                Este libro es un tratado de teoría de éticas, muy popular durante el Renacimiento.
+                                                                La primera linea del Lorem Ipsum, "Lorem ipsum dolor sit amet..",
+                                                                viene de una linea en la sección 1.10.32 El trozo de texto estándar de Lorem Ipsum usado desde el año 1500 es reproducido debajo para aquellos interesados.
+                                                                Las secciones 1.10.32 y 1.10.33 de "de Finibus Bonorum et Malorum" por Cicero son también reproducidas en su forma original exacta,
+                                                                acompañadas por versiones en Inglés de la traducción realizada en 1914 por H. Rackham.</label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="tab-pane container" id="service-three">
                             <h1>HOLA3</h1>
